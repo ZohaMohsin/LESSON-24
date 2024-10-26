479518fff8712aa235ca580a5f0f8c5977acf397
ZohaMohsin <amnamohsin72@gmail.com>
2024-10-26 17:04:00 +0500

    project

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function myfunction(){
     var sum=a+b;
     document.getElementById("p1").innerHTML="sum of "+a+" and "+b+" is "+sum ;
 }
+function validate(e) {
+    e.preventdefault();
+    
+    const email=document.getElementById('email').value;
+    const pass=document.getElementById('password').value;
+    const age=document.getElementById('age').value;
+    const msgbox=document.getElementById('message');
+    
+    let message = '';
+
+    if(email === '') {
+        message= 'abc@gmail.com';
+        msgbox.style.color = 'red';
+    } else if (pass === '') {
+        message= 'ABC.';
+        msgbox.stylr.color = 'red';
+    } else if (age ==== '') {
+        message = '16.';
+        msgbox.style.color = 'red';
+    } else {
+        message = 'login succesful!';
+        msgBox.style.color = 'green' ;
+    }
+msgBox.innertext = message;
+}
